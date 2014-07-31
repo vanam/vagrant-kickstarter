5dc4adcc5337e986dbd6c3ace01a6d1dc3b8d87a
Brad Westfall <brad@azpixels.com>
2014-07-31 08:28:55 -0700

    slight adjustments

--- vm_provision/provision-ubuntu-13.10.sh
+++ vm_provision/provision-ubuntu-13.10.sh
@@ -9,32 +9,18 @@ apt-get update
 ln -sf /usr/share/zoneinfo/America/Phoenix /etc/localtime
 
 # Apache
-echo "Bootstrap: Installing apache..."
+echo "----- Provision: Installing apache..."
 apt-get install -y apache2 apache2-utils
 echo "ServerName localhost" > "/etc/apache2/conf-available/fqdn.conf"
 a2enconf fqdn
 a2enmod rewrite
 
-echo "Bootstrap: Setup /var/www to point to /vagrant ..."
+echo "----- Provision: Setup /var/www to point to /vagrant ..."
 rm -rf /var/www
 ln -fs /vagrant /var/www
 
-# MySQL
-echo "Bootstrap: Installing mysql-client..."
-apt-get install -y mysql-client
-
-# PHP
-echo "Bootstrap: Install PHP..."
-apt-get install -y php5 php5-mysql libapache2-mod-php5 php5-mcrypt php5-cli php5-curl php5-intl php5-gd php-pear php5-json php5-dev
-cp /vagrant/vm_provision/php.ini /etc/php5/apache2/php.ini
-
-# Misc tools
-apt-get install -y postfix
-apt-get install -y memcached php5-memcached
-apt-get install php5-imagick
-
 # Apache / Virtual Host Setup
-echo "Bootstrap: Install Host File..."
+echo "----- Provision: Install Host File..."
 cp /vagrant/vm_provision/hostfile /etc/apache2/sites-available/project.conf
 a2ensite project.conf
 
@@ -42,7 +28,5 @@ a2ensite project.conf
 apt-get -y autoremove
 
 # Restart Apache
-echo "Bootstrap: Restarting Apache..."
+echo "----- Provision: Restarting Apache..."
 service apache2 restart
-
-#update-rc.d memcached enable
