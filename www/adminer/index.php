b03495834f9916fc47c41bbeeefdb3ba55541eb7
Martin Váňa <vanam@students.zcu.cz>
2017-07-28 08:20:14 +0200

    Allow access to adminer from private network.

--- www/adminer/index.php
+++ www/adminer/index.php
@@ -3,9 +3,13 @@
 declare(strict_types=1);
 
 if (isset($_SERVER['HTTP_X_FORWARDED_FOR']) || !isset($_SERVER['REMOTE_ADDR']) ||
-	!in_array($_SERVER['REMOTE_ADDR'], ['127.0.0.1', '::1', '10.0.0.1'], true)) {
+	filter_var(
+    $_SERVER['REMOTE_ADDR'],
+    FILTER_VALIDATE_IP,
+    FILTER_FLAG_IPV4 | FILTER_FLAG_IPV6 | FILTER_FLAG_NO_PRIV_RANGE |  FILTER_FLAG_NO_RES_RANGE
+	)) {
 	header('HTTP/1.1 403 Forbidden');
-	echo 'Adminer is available only from localhost';
+	echo 'Adminer is available only from private network';
 	for ($i = 2e3; $i; $i--) {
 		echo substr(" \t\r\n", rand(0, 3), 1);
 	}
